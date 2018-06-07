52b3b5314011d6b1db68875682e70700b5d95884
Alexey Puchkov <alexey.pkv@gmail.com>
2018-06-07 09:53:21 +0300

    Add some basic sanity tests

---
+++ Tests/Unit/SessionTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace SeTaco\Unit;
+
+
+use SeTaco\BrowserAssert;
+use SeTaco\DriverConfig;
+use SeTaco\Session;
+use Unitest\Wrappers\PHPUnit\UnitestCase;
+
+
+class SessionTest extends UnitestCase
+{
+	public function createConfig(array $data = []): DriverConfig
+	{
+		return DriverConfig::parse($data);
+	}
+	
+	public function subject(DriverConfig $config): Session
+	{
+		return new Session($config);
+	}
+	
+	
+	public function test_config_ConfigReturned(DriverConfig $config): void
+	{
+		$session = $this->subject($config);
+		self::assertSame($config, $session->config());
+	}
+	
+	
+	public function test_assert_AssertObjectReturned(Session $subject): void
+	{
+		self::assertInstanceOf(BrowserAssert::class, $subject->assert());
+	}
+	
+	public function test_assert_SameObjectReturnedEachTime(Session $subject): void
+	{
+		self::assertSame($subject->assert(), $subject->assert());
+	}
+}
